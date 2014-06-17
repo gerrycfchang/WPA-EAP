af595c6c3ee3ea52fb646ca7f4e23927d9468035
gerrychchang <alfie.gerrycheung@gmail.com>
2014-06-17 12:26:12 +0800

    add 2 test cases about v2 and cert

---
+++ test_enable_wifi_TTLS_V2_with_cert.py
@@ -0,0 +1,40 @@
+from gaiatest import GaiaTestCase
+from gaiatest.apps.settings.app import Settings
+from marionette.by import By
+import sys
+
+class TestWpaWlan(GaiaTestCase):       
+    
+    def setUp(self):
+        GaiaTestCase.setUp(self)  
+        sys.path.append("./")          
+
+    def test_enable_wifi(self):
+        settings = Settings(self.marionette)
+        settings.launch()
+        wifiObj = settings.open_wifi_settings()
+        
+        _wifi_enabled_checkbox_locator = (By.CSS_SELECTOR, '#wifi-enabled input')
+        
+        checkbox = self.marionette.find_element(*_wifi_enabled_checkbox_locator)
+        if not checkbox.is_selected():
+            wifiObj.enable_wifi()
+            
+        import WPA
+        wpaObj = WPA.WpaEap(self.marionette)
+        wpaObj.selectWPANetwork('TPE_QA')
+        wpaObj.selectEAPMethod('TTLS')
+        wpaObj.choosePhase2Auth('MSCHAP V2')
+        wpaObj.inputIdentity('sqa')
+        wpaObj.inputPassword('password')
+        wpaObj.selectServerCertificate('cacert')
+        wpaObj.join()
+        
+        networkName = wpaObj.getActiveNetworkName()        
+        self.assertEqual(networkName, 'TPE_QA')
+        
+        networkStatus = wpaObj.getActiveNetworkStatus()
+        self.assertEqual(networkStatus, 'Connected')
+                
+        #forget wifi network
+        wpaObj.forgetNetwork('TPE_QA')
