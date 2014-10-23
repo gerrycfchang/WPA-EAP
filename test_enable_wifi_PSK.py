95e096420ebd86558c0270b42208fd20ec6f036a
gerrycfchang <alfie.gerrycheung@gmail.com>
2014-10-23 15:33:14 +0800

    remove marks

--- test_enable_wifi_PSK.py
+++ test_enable_wifi_PSK.py
@@ -9,18 +9,7 @@ class TestWpaWlan(GaiaTestCase):
         sys.path.append("./tests/functional/WPA-EAP")
 
     def test_enable_wifi(self):
-        ''' v2.0
-        settings = Settings(self.marionette)
-        settings.launch()
-        wifiObj = settings.open_wifi_settings()
-        
-        _wifi_enabled_checkbox_locator = (By.CSS_SELECTOR, '#wifi-enabled input')
-        
-        checkbox = self.marionette.find_element(*_wifi_enabled_checkbox_locator)
-        if not checkbox.is_selected():
-            wifiObj.enable_wifi()        
-        '''
-        
+ 
         import WPA
         # v2.1
         wpaObj = WPA.WpaEap(self.marionette)
@@ -32,13 +21,7 @@ class TestWpaWlan(GaiaTestCase):
         wpaObj.join()                
       
         networkName = wpaObj.getActiveNetworkName()
-        ''' v2.0
-        self.assertEqual(networkName, 'Mozilla Mobile')
-      
-        networkStatus = wpaObj.getActiveNetworkStatus()
-        self.assertEqual(networkStatus, 'Connected')
-        '''
-        #v2.1
+
         self.assertIn('Mozilla Mobile',networkName)
         
         method = wpaObj.getStatusSecurityMethod()
